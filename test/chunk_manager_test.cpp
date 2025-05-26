95add0b04b44e5af93393f3f3d7e9cd826ca6012
FinleyConway <finleyconway03@gmail.com>
2025-05-26 12:34:07 +0100

    Add catch2 unit testing

---
+++ test/chunk_manager_test.cpp
@@ -0,0 +1,14 @@
+#include <catch2/catch_test_macros.hpp>
+#include "chunk_manager.h"
+
+TEST_CASE("Chunk Manager Class Test", "[ChunkManager]")
+{
+    ChunkManager manager;
+
+    SECTION("Get cell from chunk")
+    {
+        // need a unique ChunkContext for testing
+
+        REQUIRE(true);
+    }
+}
